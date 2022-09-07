47f9629c520f7031cb458402ed1dd46ff3340bc2
Magdalena Kierat <m.kierat@gmx.de>
2022-09-07 19:06:06 +0200

    add basic validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,6 +10,13 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      // + converts const into number
+      return;
+    }
     setEnteredUsername("");
     setEnteredAge("");
   };
